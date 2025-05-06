eeec39df9380d79ad160f3404c8dc319dabf36f0
NASRUM SHARON <119867650+nshoron@users.noreply.github.com>
2025-05-07 01:46:40 +0600

    Add files via upload

---
+++ Inheritence/Class_Object/Class_Object/Tutor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object
+{
+    class Tutor
+    {
+        int TutorID;
+        string TutorName;
+
+        public void SetTutor(int id, string name)
+        {
+            TutorID = id;
+            TutorName = name;
+        }
+        public int GetTutorID()
+        {
+            return TutorID;
+        }
+        public string GetTutorName()
+        {
+            return TutorName;
+        }
+        static void Main(string[] args)
+        {
+            Tutor t = new Tutor();
+            t.SetTutor(101,"K.B.M");
+            t.GetTutorID();
+            Console.WriteLine(t.GetTutorID());
+            Console.WriteLine(t.GetTutorName()+" sir");
+            Console.ReadKey();
+        }
+    }
+}
