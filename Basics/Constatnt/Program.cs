e3c79959ab43c398eaedde9adbbf91def6ac720d
NASRUM SHARON <119867650+nshoron@users.noreply.github.com>
2025-05-07 01:24:23 +0600

    Add files via upload

---
+++ Basics/Constatnt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1416;
+            Console.Write("Enter Radius : ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = pi * radius * radius;
+            Console.WriteLine("Area is  : " + area);
+            Console.ReadLine();
+        }
+    }
+}
